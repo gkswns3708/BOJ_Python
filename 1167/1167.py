5d89a81925838cf73c294e647050ad8f85a77dee
gkswns3708 <gkswns3708@naver.com>
2022-04-12 02:44:27 +0900

    BOJ

---
+++ 1167/1167.py
@@ -0,0 +1,62 @@
+import sys
+from collections import deque
+
+get_line: iter = lambda: map(int, sys.stdin.readline().rstrip().split())
+get_input: int = lambda: int(sys.stdin.readline().strip())
+
+
+V = None
+adj = None
+visited = None
+dist = None
+
+
+def set_variable():
+    global V, adj, visited, dist
+    V = get_input()
+    adj = [[] for _ in range(V + 1)]
+    visited = [False for _ in range(V + 1)]
+    dist = [sys.maxsize for _ in range(V + 1)]
+    for _ in range(V):
+        line = list(get_line())
+        st = line[0]
+        for idx in range(1, len(line) - 1, 2):
+            nxt, weight = line[idx], line[idx + 1]
+            adj[st].append((nxt, weight))  # a -> b와 b -> a 모두 들어옴.
+
+
+def solution():
+    global V, adj, visited
+
+    def BFS(st_idx):
+        # init variable
+        global V, adj, visited
+        visited = [False for _ in range(V + 1)]
+        dist = [sys.maxsize for _ in range(V + 1)]
+
+        que = deque()
+        que.append((st_idx, 0))
+        visited[st_idx] = True
+        dist[st_idx] = 0
+
+        while que:
+            now_pos, now_dist = que.popleft()
+
+            for nxt_pos, weight in adj[now_pos]:
+                if visited[nxt_pos] == False:
+                    visited[nxt_pos] = True
+                    que.append((nxt_pos, now_dist + weight))
+                    dist[nxt_pos] = now_dist + weight
+        return dist
+
+    temp = BFS(1)
+    temp_V = temp.index(max(temp[1:]))
+
+    temp = BFS(temp_V)
+    ans = max(temp[1:])
+    print(ans)
+
+
+if __name__ == "__main__":
+    set_variable()
+    solution()
