ed0dc006f6ea411dd1de6245f986948ba10bb50d
gkswns3708 <gkswns3708@naver.com>
2022-05-04 13:51:17 +0900

    1717

---
+++ 1717/1717.py
@@ -0,0 +1,12 @@
+import sys
+
+get_line: iter = lambda: map(int, sys.stdin.readline().rstrip().split())
+get_input: int = lambda: int(sys.stdin.readline().strip())
+
+def set_variable():
+
+def solution():
+
+if __name__ == "__main__":
+    set_variable()
+    solution()
